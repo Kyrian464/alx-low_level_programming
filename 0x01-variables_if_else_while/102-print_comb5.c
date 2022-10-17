376826a02dc35c8c267c09de41937cc45d69c6a6
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-17 09:58:59 -0700

    print all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,26 +8,27 @@
 
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int i, j;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (i = 0; i < 100; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		for (j = 0; j < 100; j++)
 		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			if (i < j)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
-
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				putchar(',');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
