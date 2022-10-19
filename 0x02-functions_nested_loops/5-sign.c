d340a84ea0b07717b1634c101a54b86e4eb405c5
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-19 07:29:43 -0700

    print sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - program that prints sign of a number
+ * @n: parameter to be checked
+ * Return: returns + if positive
+ *         - if negative and
+ *         0 if zero
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	print_sign('+');
+	return (1);
+
+	else if (n < 0)
+	print_sign('-');
+	return ('-'1);
+
+	else
+	print_sign(0);
+	return (0);
+}
