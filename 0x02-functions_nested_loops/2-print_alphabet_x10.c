9e12035e679428fd1b9d921219f75395d9655339
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-18 17:40:27 -0700

    printb alpha 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print apha in lower case ten times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9;)
+	{
+		for (letter = 'a'; letter <= 'z';)
+
+			_putchar(letter);
+	}
+
+	_putchar('\n');
+}
