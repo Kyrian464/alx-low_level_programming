0936e2b9091d1fafcbb191d859f9cdd9929a575c
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-19 07:47:18 -0700

    print sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,15 +13,15 @@ int print_sign(int n)
 	{
 
 	if (n > 0)
-	print_sign('+');
+	_putchar('+');
 	return (1);
 
 	if (n < 0)
-	print_sign('-');
+	_putchar('-');
 	return (-1);
 
 	if (n == 0)
-	print_sign(0);
+	_putchar(0);
 	return (0);
 	}
 }
