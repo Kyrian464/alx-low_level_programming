367072bc9d20def681aa21f72ca67690e297224c
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-20 05:59:06 -0700

    test positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** positive_or_negative - funtion that tests positive or negative
+ *
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
