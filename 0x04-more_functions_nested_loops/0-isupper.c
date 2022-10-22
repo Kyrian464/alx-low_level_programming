cfc9eb2e39c267c0cbe46f7e72314da4f1379c0a
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-21 17:30:04 -0700

    check uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,17 @@
 /**
  * _isupper - function that checks for uppercase character
  * @c: parameter to be checked
- * Return: 0 if upper case and 1 otherwise
+ * Return: 1 if upper case and 0 otherwise
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
