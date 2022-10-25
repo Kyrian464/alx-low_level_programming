4db5f6f7f2359e8e00317cf7e32d7547ada31463
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-25 07:25:38 -0700

    reset value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*
-*  Return: 0 is success
-*/
+ * reset_to_98 - reset value of pointer to 98
+ * @n: pointer variable
+ *
+ * Return: 0
+ */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-}
-Footer
 
+	return (0);
+}
