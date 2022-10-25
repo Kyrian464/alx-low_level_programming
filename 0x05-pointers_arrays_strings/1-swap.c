2fad8e626e5186c3518fd3eeb40af119fe848e48
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-10-25 07:46:58 -0700

    swap value

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* swap_int - swaps the value of int a and int b 
+* swap_int - swaps the value of int a and int b
 * @a: first int
 * @b: second int
 * Return 0
@@ -12,4 +12,4 @@ void swap_int(int *a, int *b)
 		s = *a;
 		*a = *b;
 		*b = s;
-} 
+}
