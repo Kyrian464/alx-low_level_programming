387bc596b38bc1e833c5483e40d32b4e9519cd8b
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-11-13 14:36:21 -0800

    print count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: - argument count
+ * @argv: - argument array of strings
+ *
+ * Return: 0 is success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
