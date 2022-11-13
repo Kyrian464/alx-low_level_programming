9846eee96175421f3454a06d086dc46f90df6a1a
Kyrian Chuks Chukwuezi <chukwuezikc14464@gmail.com>
2022-11-13 14:13:09 -0800

    print argc equals 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: - 0 is success
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(*argv[0]);
+	_putchar('\n');
+	(void)argc;
+
+	return (0);
+}
